93652839f1223663ff3d4a127849293038522fd0
jbkr <jb_kr@yahoo.co.kr>
2024-01-27 12:11:36 +0900

    first

---
+++ src/test/java/jpa/practice/ver1/JpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.practice.ver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
