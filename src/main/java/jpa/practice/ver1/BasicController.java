d4374309215ceb32c9dd37d48488036877c49bdf
jbkr <jb_kr@yahoo.co.kr>
2024-01-27 14:41:08 +0900

    second

---
+++ src/main/java/jpa/practice/ver1/BasicController.java
@@ -0,0 +1,14 @@
+package jpa.practice.ver1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasicController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+}
