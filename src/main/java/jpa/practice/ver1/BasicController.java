c8726358026460c8c221c1803266c7be726d6f34
jbkr <jb_kr@yahoo.co.kr>
2024-01-27 14:52:29 +0900

    third

--- src/main/java/jpa/practice/ver1/BasicController.java
+++ src/main/java/jpa/practice/ver1/BasicController.java
@@ -2,6 +2,7 @@ package jpa.practice.ver1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BasicController {
@@ -9,6 +10,21 @@ public class BasicController {
 	@GetMapping("/")
 	public String home() {
 		return "home";
+	}
+	
+	@GetMapping("/add")
+	public String addForm() {
+		return "add";
+	}
+	
+	@GetMapping("/list")
+	public String list() {
+		return "list";
+	}
+	
+	@PostMapping("/add")
+	public String add() {
+		return "redirect:/";
 	}
 	
 }
