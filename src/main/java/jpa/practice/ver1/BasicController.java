298fbbd7acb704b580a73d9dfd19636ac2d5274d
jbkr <jb_kr@yahoo.co.kr>
2024-01-27 15:16:51 +0900

    fourth

--- src/main/java/jpa/practice/ver1/BasicController.java
+++ src/main/java/jpa/practice/ver1/BasicController.java
@@ -2,29 +2,40 @@ package jpa.practice.ver1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BasicController {
 
+	private final ItemService itemService;
+
+	public BasicController(ItemService itemService) {
+		this.itemService = itemService;
+	}
+
 	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
-	
+
 	@GetMapping("/add")
 	public String addForm() {
 		return "add";
 	}
-	
+
 	@GetMapping("/list")
 	public String list() {
 		return "list";
 	}
-	
+
 	@PostMapping("/add")
-	public String add() {
+	public String add(@ModelAttribute ItemDTO itemDTO) {
+		Item item = new Item();
+		item.setName(itemDTO.getName());
+		item.setPrice(itemDTO.getPrice());
+		itemService.addItem(item);
 		return "redirect:/";
 	}
-	
+
 }
